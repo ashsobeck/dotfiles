d04abb364b882017f925d4ffd68f8afeddfc73f3
Ashton Sobeck <asobeck@g.clemson.edu>
2023-08-13 09:41:51 -0400

    updates

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -18,12 +18,12 @@ require('lualine').setup {
         }
     },
     sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
     },
     inactive_sections = {
         lualine_a = {},
@@ -35,16 +35,19 @@ require('lualine').setup {
     },
     tabline = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = {
             {
                 'filename',
-                path = 2
+                path = 3,
+                symbols = {
+                    modified = ' ●'
+                }
             }
         },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
 
     },
     winbar = {},
--- lua/ashton/set.lua
+++ lua/ashton/set.lua
@@ -26,6 +26,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
+-- vim.opt.clipboard = "unnamedplus"
 
 vim.opt.updatetime = 50
 
