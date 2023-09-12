056d1402464b65a19a3e73100517427c86d5ddd8
Ashton Sobeck <asobeck99@gmail.com>
2023-09-12 19:42:42 -0400

    feat: make type changes reflect in .svelte files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,14 @@ lsp.on_attach(function(client, bufnr)
     lsp.buffer_autoformat()
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
+    if client.name == 'svelte' then
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+            end,
+        })
+    end
 end)
 
 -- (Optional) Configure lua language server for neovim
